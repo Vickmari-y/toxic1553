af1f27d6df73154deed14d4dafbaf099c78ed024
Ivan Pikulin <ivan.pikulin@chemistry.msu.ru>
2024-02-17 10:01:37 +0300

    add ReduceLROnPlateau

--- optimize_hparams.py
+++ optimize_hparams.py
@@ -1,13 +1,13 @@
-import numpy as np
 import optuna
 import pandas as pd
 import torch
-from deepchem.feat import RDKitDescriptors, CircularFingerprint, MolecularFeaturizer
+from deepchem.feat import RDKitDescriptors, CircularFingerprint
 from rdkit import Chem
 from sklearn.model_selection import train_test_split
 from torch import nn
 from torch.optim import Adam, AdamW, SGD, RMSprop
-from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.data import DataLoader, TensorDataset
 from tqdm import tqdm
 
 from Model import FeedForward
@@ -57,23 +57,25 @@ def estimate_params(trial):
     }
 
 
-def load_data(filename, featurizer, batch_size=16, test_size=0.1):
+def load_data(filename, featurizer, batch_size=16, test_size=0.1, val_size=0.1):
     df = pd.read_csv(filename)
     molecules = [Chem.MolFromSmiles(s) for s in df["smiles"]]
     targets = torch.tensor(df["value"].tolist(), dtype=torch.float32)
 
     X_full = featurizer.featurize(molecules)
     X_full = torch.from_numpy(X_full).to(torch.float32)
-    x_train, x_test, y_train, y_test = train_test_split(X_full, targets, test_size=test_size, random_state=42)
+    x_train_val, x_test, y_train_val, y_test = train_test_split(X_full, targets, test_size=test_size, random_state=42)
+    x_train, x_val, y_train, y_val = train_test_split(x_train_val, y_train_val, test_size=val_size, random_state=42)
 
-    train_data = list(zip(x_train, y_train))
-    train_dataloader = DataLoader(train_data, batch_size=batch_size)
-    test_data = list(zip(x_test, y_test))
+    train_dataloader = DataLoader(TensorDataset(x_train, y_train), batch_size=batch_size)
+    test_dataloader = DataLoader(TensorDataset(x_test, y_test), batch_size=batch_size)
+    val_dataloader = DataLoader(TensorDataset(x_val, y_val), batch_size=batch_size)
 
-    return train_dataloader, test_data
+    return train_dataloader, val_dataloader, test_dataloader
 
 
-def train_model(model, optimizer, loss_function, train_dataloader, test_data):
+def train_model(model, optimizer, loss_function, train_dataloader, val_dataloader, test_dataloader):
+    scheduler = ReduceLROnPlateau(optimizer, patience=30, factor=0.1)
     for epoch in tqdm(range(max_epoch), desc="Training progress", total=max_epoch):
         for i, batch in enumerate(train_dataloader):
             x, y_true = batch
@@ -83,28 +85,35 @@ def train_model(model, optimizer, loss_function, train_dataloader, test_data):
             loss.backward()
             optimizer.step()
 
-    test_predictions = torch.tensor([model(x).item() for x, y in test_data])
-    test_loss = loss_function(test_predictions, torch.tensor([y for x, y in test_data]))
+            val_predictions = torch.cat([model(x) for x, y in val_dataloader], dim=0)
+            val_true = torch.cat([y for x, y in val_dataloader], dim=0)
+            val_loss = loss_function(val_predictions, val_true)
+
+            scheduler.step(val_loss)
+
+    test_predictions = torch.cat([model(x) for x, y in test_dataloader], dim=0)
+    test_true = torch.cat([y for x, y in test_dataloader], dim=0)
+    test_loss = loss_function(test_predictions, test_true)
 
     return test_loss.item()
 
 
 def objective(trial):
     featurizer_name = trial.suggest_categorical("featurizer", featurizer_variants.keys())
-    train_dataloader, test_data = load_data(
+    train_dataloader, val_dataloader, test_dataloader = load_data(
         filename,
         featurizer=featurizer_variants[featurizer_name],
         batch_size=trial.suggest_int("batch_size", 16, 256, log=True),
         test_size=0.1)
     params = estimate_params(trial)
-    model = FeedForward(**params, num_in_features=test_data[0][0].shape[-1], num_out_features=1)
+    model = FeedForward(**params, num_in_features=next(iter(test_dataloader))[0].shape[-1], num_out_features=1)
 
     learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-1, log=True)
     optimizer_name = trial.suggest_categorical("optimizer", optimizer_variants.keys())
     optimizer = optimizer_variants[optimizer_name](model.parameters(), lr=learning_rate)
 
     loss_function = nn.MSELoss()
-    final_test_loss = train_model(model, optimizer, loss_function, train_dataloader, test_data)
+    final_test_loss = train_model(model, optimizer, loss_function, train_dataloader, val_dataloader, test_dataloader)
 
     return final_test_loss
 
